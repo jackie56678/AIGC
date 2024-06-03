43c33c8470cfc9b72b58dfc8a5d34396f6e0e565
jackie56678 <jack666ie@gmail.com>
2024-06-03 15:50:39 +0800

    update

--- src/backend/start.sh
+++ src/backend/start.sh
@@ -2,10 +2,10 @@
 
 # 启动 Java 应用程序
 cd /ssadmin-server
-java ${JAVA_OPTS} -jar app.jar $ARGS
+java ${JAVA_OPTS} -jar app.jar $ARGS &
 
 # 启动 Python HTTP 服务器
 cd /ssadmin-server/source
-python3 -m http.server 8000 &
+python3 -m http.server 8000
 
 
