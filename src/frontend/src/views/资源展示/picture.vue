9f8cf9075910c62c676d38669a649f6259bc742e
jackie56678 <jack666ie@gmail.com>
2024-06-03 19:05:34 +0800

    update

--- src/frontend/src/views/资源展示/picture.vue
+++ src/frontend/src/views/资源展示/picture.vue
@@ -40,7 +40,7 @@ export default {
         .then(response => {
           console.log(response.data.data)
           // const baseURL = 'http://localhost:8000/picture/';
-          const baseURL = 'http://47.102.159.229:9000/picture/';
+          const baseURL = 'app/picture/';
           for (let i = 0; i < response.data.data.length; i++) {
               response.data.data[i] = baseURL + response.data.data[i];
           }
