498228c4a6c69964eca1ddfdd948656f65065c19
jackie56678 <jack666ie@gmail.com>
2024-06-03 23:00:19 +0800

    update

--- src/backend/start.sh
+++ src/backend/start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-yum install net-tools lsof
+# yum install net-tools lsof
 
 # 启动 Java 应用程序
 cd /ssadmin-server
 java ${JAVA_OPTS} -jar app.jar $ARGS &
 
 # 启动 Python HTTP 服务器
-#cd /ssadmin-server/source
-#python3 -m http.server 9000
+cd /ssadmin-server/source
+python3 -m http.server 9000
 
 
--- start.sh
+++ start.sh
@@ -8,6 +8,4 @@ docker run -it --rm --name ssadmin-maven \
     jack666ie/javamaven:21396 mvn clean install package -e -Dmaven.test.skip=true
 
 docker compose up -d
-
-cd /source
-python3 -m http.server 9000
+docker ps -a
